47172674d66a02e35f3313b577e6e0234c2c3bd8
Lucas Merencia <lucas.merencia@gmail.com>
2016-02-03 22:50:07 -0200

    testing tasks with exceptions

---
+++ test/task-fail-test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var expect = require('expect.js');
+var sinon = require('sinon');
+var cron = require('../src/cron.js');
+
+describe('scheduling a task with exception', function(){
+  beforeEach(function(){
+    this.clock = sinon.useFakeTimers();
+  });
+
+  afterEach(function(){
+    this.clock.restore();
+  });
+
+  it('should not stop on task exception', function() {
+    var executed = 0;
+    cron.schedule('* * * * *', function(){
+      executed += 1;
+      throw 'exception!'
+    });
+    this.clock.tick(3000 * 60);
+    expect(executed).to.equal(3);
+  });
+});
