8b435bde1b3b8b3b3c5187a5a7330a85adeba2f5
David <david.recuencogadea@gmail.com>
2016-02-11 19:17:31 +0100

    ADD stop cron task

--- src/node-cron.js
+++ src/node-cron.js
@@ -2,14 +2,35 @@
 
 var Task = require('./task');
 
-module.exports = (function(){
-  function createTask(expression, func){
+module.exports = (function() {
+
+  /**
+   * Creates a new task to execute given function when the cron
+   *  expression ticks.
+   *
+   * @param {string} expression - cron expression.
+   * @param {Function} func - task to be executed.
+   * @returns {intervalObject} update function.
+   */
+  function createTask(expression, func) {
     var task = new Task(expression, func);
-    setInterval(function(){
+
+    return setInterval(function() {
       task.update(new Date());
     }, 1000);
   }
-  return {
-    schedule: createTask
+
+  /**
+   * Stops a cron task.
+   *
+   * @param {intervalObject} task - task to be stopped.
+   */
+  function stopTask(task) {
+    clearInterval(task);
   }
+
+  return {
+    schedule: createTask,
+    stop: stopTask
+  };
 }());
