052f0156ea1ea6549bcd8458be7181eefcece341
Lucas Merencia <lucas.merencia@gmail.com>
2016-05-21 19:45:47 -0300

    removing unused step validation

--- src/pattern-validation.js
+++ src/pattern-validation.js
@@ -3,18 +3,7 @@
 var convertExpression = require('./convert-expression');
 
 module.exports = ( function(){
-
-  function removeStepValue(expression){
-    var multiplePattern = /^((\d+(\,\d+){0,})|\*)\/(\d+)$/g;
-    var match = multiplePattern.exec(expression);
-    if(match !== null && match.length > 0 ) {
-      expression = match[1];
-    }
-    return expression;
-  }
-
   function isValidExpression(expression, min, max){
-    expression = removeStepValue(expression);
     var options = expression.split(',');
     var regexValidation = /^\d+$|^\*$|^\*\/\d+$/;
     for(var i = 0; i < options.length; i++){
