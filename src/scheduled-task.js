0317e65aee1321130089318da5704e500e72549c
merencia <lucas.merencia@gmail.com>
2022-01-26 15:57:25 -0300

    lint

--- src/scheduled-task.js
+++ src/scheduled-task.js
@@ -1,50 +1,50 @@
-"use strict";
+'use strict';
 
-const EventEmitter = require("events");
-const Task = require("./task");
-const Scheduler = require("./scheduler");
+const EventEmitter = require('events');
+const Task = require('./task');
+const Scheduler = require('./scheduler');
 
 class ScheduledTask extends EventEmitter {
-  constructor(cronExpression, func, options) {
-    super();
-    if (!options) {
-      options = {
-        scheduled: true,
-        recoverMissedExecutions: false,
-      };
+    constructor(cronExpression, func, options) {
+        super();
+        if (!options) {
+            options = {
+                scheduled: true,
+                recoverMissedExecutions: false,
+            };
+        }
+        let task = new Task(func);
+        let scheduler = new Scheduler(
+            cronExpression,
+            options.timezone,
+            options.recoverMissedExecutions
+        );
+
+        scheduler.on('scheduled-time-matched', (now) => {
+            let result = task.execute(now);
+            this.emit('task-done', result);
+        });
+
+        if (options.scheduled !== false) {
+            scheduler.start();
+        }
+
+        this.start = () => {
+            scheduler.start();
+        };
+
+        this.stop = () => {
+            scheduler.stop();
+        };
+
+        this.destroy = () => {
+            return scheduler.destroy();
+        };
+
+        this.getStatus = () => {
+            return scheduler.getStatus();
+        };
     }
-    let task = new Task(func);
-    let scheduler = new Scheduler(
-      cronExpression,
-      options.timezone,
-      options.recoverMissedExecutions
-    );
-
-    scheduler.on("scheduled-time-matched", (now) => {
-      let result = task.execute(now);
-      this.emit("task-done", result);
-    });
-
-    if (options.scheduled !== false) {
-      scheduler.start();
-    }
-
-    this.start = () => {
-      scheduler.start();
-    };
-
-    this.stop = () => {
-      scheduler.stop();
-    };
-
-    this.destroy = () => {
-      return scheduler.destroy();
-    };
-
-    this.getStatus = () => {
-      return scheduler.getStatus();
-    };
-  }
 }
 
 module.exports = ScheduledTask;
--- src/scheduler.js
+++ src/scheduler.js
@@ -1,67 +1,67 @@
-"use strict";
+'use strict';
 
-const EventEmitter = require("events");
-const TimeMatcher = require("./time-matcher");
+const EventEmitter = require('events');
+const TimeMatcher = require('./time-matcher');
 
 class Scheduler extends EventEmitter {
-  constructor(pattern, timezone, autorecover) {
-    super();
-    this.timeMatcher = new TimeMatcher(pattern, timezone);
-    this.autorecover = autorecover;
-  }
+    constructor(pattern, timezone, autorecover) {
+        super();
+        this.timeMatcher = new TimeMatcher(pattern, timezone);
+        this.autorecover = autorecover;
+    }
 
-  start() {
+    start() {
     // clear timeout if exsits
-    this.stop();
+        this.stop();
 
-    let lastCheck = process.hrtime();
-    let lastExecution = this.timeMatcher.apply(new Date());
+        let lastCheck = process.hrtime();
+        let lastExecution = this.timeMatcher.apply(new Date());
 
-    const matchTime = () => {
-      const delay = 1000;
-      const elapsedTime = process.hrtime(lastCheck);
-      const elapsedMs = (elapsedTime[0] * 1e9 + elapsedTime[1]) / 1e6;
-      const missedExecutions = Math.floor(elapsedMs / 1000);
+        const matchTime = () => {
+            const delay = 1000;
+            const elapsedTime = process.hrtime(lastCheck);
+            const elapsedMs = (elapsedTime[0] * 1e9 + elapsedTime[1]) / 1e6;
+            const missedExecutions = Math.floor(elapsedMs / 1000);
 
-      for (let i = missedExecutions; i >= 0; i--) {
-        const date = new Date(new Date().getTime() - i * 1000);
-        let date_tmp = this.timeMatcher.apply(date);
-        if (
-          lastExecution.getTime() < date_tmp.getTime() &&
+            for (let i = missedExecutions; i >= 0; i--) {
+                const date = new Date(new Date().getTime() - i * 1000);
+                let date_tmp = this.timeMatcher.apply(date);
+                if (
+                    lastExecution.getTime() < date_tmp.getTime() &&
           (i === 0 || this.autorecover) &&
           this.timeMatcher.match(date)
-        ) {
-          this.emit("scheduled-time-matched", date_tmp);
-          date_tmp.setMilliseconds(0);
-          lastExecution = date_tmp;
-        }
-      }
-      lastCheck = process.hrtime();
-      this.timeout = setTimeout(matchTime, delay);
-    };
-    matchTime();
-  }
-
-  stop() {
-    if (this.timeout) {
-      clearTimeout(this.timeout);
+                ) {
+                    this.emit('scheduled-time-matched', date_tmp);
+                    date_tmp.setMilliseconds(0);
+                    lastExecution = date_tmp;
+                }
+            }
+            lastCheck = process.hrtime();
+            this.timeout = setTimeout(matchTime, delay);
+        };
+        matchTime();
     }
-    this.timeout = null;
-  }
 
-  destroy() {
-    return new Promise((res, rej) => {
-      this.stop();
-      setTimeout(() => {
-        if (!this.getStatus()) return res(true);
-        return rej(false);
-      }, 15);
-    });
-  }
+    stop() {
+        if (this.timeout) {
+            clearTimeout(this.timeout);
+        }
+        this.timeout = null;
+    }
 
-  getStatus() {
-    return this.timeout && !this.timeout._destroyed;
-  }
+    destroy() {
+        return new Promise((res, rej) => {
+            this.stop();
+            setTimeout(() => {
+                if (!this.getStatus()) return res(true);
+                return rej(false);
+            }, 15);
+        });
+    }
+
+    getStatus() {
+        return this.timeout && !this.timeout._destroyed;
+    }
 }
 
 module.exports = Scheduler;
