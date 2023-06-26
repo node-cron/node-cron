5b19d37a277c18f270d93e2509064d6641b7ba50
Tamil Selvan Elangovan <tamilselvan.e@quadkast.co.in>
2023-06-27 02:23:39 +0530

    CircleCI bug fixes

--- src/task.js
+++ src/task.js
@@ -16,13 +16,13 @@ class Task extends EventEmitter{
         try {
             exec = this._execution(now);
         } catch (error) {
-            return this.emit('task-failed', {error, now});
+            return this.emit('task-failed', {...error, now});
         }
         
         if (exec instanceof Promise) {
             return exec
                 .then(() => this.emit('task-finished', {now}))
-                .catch((error) => this.emit('task-failed', {error, now}));
+                .catch((error) => this.emit('task-failed', {...error, now}));
         } else {
             this.emit('task-finished', {now});
             return exec;
