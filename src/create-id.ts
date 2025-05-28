f569e0184d39cf32717e0862476f8f88d3deeed5
merencia <lucas@merencia.com>
2025-05-28 11:10:05 -0300

    fix: create id function making it compatible with old node version like node 16

--- src/create-id.ts
+++ src/create-id.ts
@@ -2,9 +2,7 @@ import crypto from 'node:crypto';
 
 export function createID(prefix: string = '', length: number = 16): string {
   const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const values = new Uint8Array(length);
-  crypto.getRandomValues(values);
+  const values = crypto.randomBytes(length);
   const id = Array.from(values, v => charset[v % charset.length]).join('');
-  if(prefix) return `${prefix}-${id}`;
-  return id;
+  return prefix ? `${prefix}-${id}` : id;
 }
